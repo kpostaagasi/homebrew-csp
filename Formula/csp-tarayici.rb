30a4057490225a7bef7a63087df922cb54ed60f3
Kamil Postaagasi <kamilpostaagasi@gmail.com>
2026-09-08 13:34:49 +0300

    csp-tarayici 0.2.0: virtualenv install of the package

--- Formula/csp-tarayici.rb
+++ Formula/csp-tarayici.rb
@@ -1,23 +1,22 @@
 class CspTarayici < Formula
-  include Language::Python::Shebang
+  include Language::Python::Virtualenv
 
   desc "Cash-secured put scanner with a curses TUI, no API keys"
   homepage "https://github.com/kpostaagasi/csp-tarayici"
-  url "https://github.com/kpostaagasi/csp-tarayici/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "560cbb115aec34e1fa0844d5eef19e718b43632d26bfffef19ff02dfdc636ace"
+  url "https://github.com/kpostaagasi/csp-tarayici/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "15533c4367b4599d1b661eb62b8e17f1a7c539387089be5df3200c86b79cdee2"
   license "MIT"
 
   depends_on "python@3.13"
 
+  # No resource blocks: the package has zero runtime dependencies.
   def install
-    rewrite_shebang detected_python_shebang, "csp.py"
-    bin.install "csp.py" => "csp"
-    doc.install "README.md"
+    virtualenv_install_with_resources
   end
 
   test do
-    assert_match "selftest ok", shell_output("#{bin}/csp --selftest")
     assert_match version.to_s, shell_output("#{bin}/csp --version")
     assert_match "sembol yok", shell_output("#{bin}/csp 2>&1", 1)
+    assert_match "kayıtlı zincir yok", shell_output("#{bin}/csp --replay NOK 2>&1", 1)
   end
 end
